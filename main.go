871bf2b8aa5d97da0adcd850140d85238eaf61a2
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-14 12:50:58 +0200

    Implement routing

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func getComments(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("here you'll get the comments"))
+}
+
+func postComments(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("thank you for posting a comment"))
+}
+
+func main() {
+	http.HandleFunc("GET /comments", getComments)
+	http.HandleFunc("POST /comments", postComments)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
