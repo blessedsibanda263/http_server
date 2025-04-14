bdeb5a8c7ea8edf7f7870bc09d738263e9b4b8e9
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-04-14 13:57:38 +0200

    Restructure

---
+++ examples/comments.go
@@ -0,0 +1,44 @@
+package examples
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type comment struct {
+	text       string
+	dateString string
+}
+
+var comments []comment
+
+func getComments(w http.ResponseWriter, r *http.Request) {
+	commentBody := ""
+	for i := range comments {
+		commentBody += fmt.Sprintf("%s (%s)\n", comments[i].text, comments[i].dateString)
+	}
+	fmt.Fprintf(w, "%s", fmt.Sprintf("Comments: \n%s", commentBody))
+}
+
+func postComments(w http.ResponseWriter, r *http.Request) {
+	commentText, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	comments = append(comments, comment{
+		text:       string(commentText),
+		dateString: time.Now().Format(time.RFC3339),
+	})
+	w.WriteHeader(http.StatusOK)
+}
+
+func CommentsServer() {
+	http.HandleFunc("GET /comments", getComments)
+	http.HandleFunc("POST /comments", postComments)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
